7d2ff3540ed9ee048f4f0609dbfcae870082486e
Lambert <ljh4timm@xs4all.nl>
2020-05-16 20:55:15 +0200

    Update pru.c
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/pru.c
+++ src/pru.c
@@ -22,3 +22,38 @@
 /* Driver header file. */
 #include "prussdrv.h"
 #include <pruss_intc_mapping.h>
+
+
+/* Local Macro Declarations. */
+#define PRU_NUM 1
+#define OFFSET_MEM0 0x00002000
+#define OFFSET_SHAREDRAM 0x00010000
+
+
+/* Local Function Declarations. */
+
+/* Global Variable Definitions. */
+static void *pruDataMem;
+static unsigned char *pruDataMem_short0; /* AM33XX_DATA 8KB RAM0 */
+static unsigned char *pruDataMem_short1; /* AM33XX_DATA 8KB RAM1 */
+int num = 4096;
+
+/* Global Function Definitions. */
+
+char convertdata (unsigned char d)
+{
+  /* Convert 2-complementary binary data to decimal. */
+  int sign;
+  char converted;
+  sign = (d >> 7) & 0x01;
+  if (sign == 1)
+  {
+    converted = d - 128; /* Negative. */
+  }
+  else
+  {
+    converted = d + 128; /* Positive. */
+  }
+  return converted;
+}
+  
