8b94632f02fcdd860efeeab42ed1d3f176515c7b
Lambert <ljh4timm@xs4all.nl>
2020-05-16 19:17:35 +0200

    Update pru.c
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/pru.c
+++ src/pru.c
@@ -6,19 +6,19 @@
  * Project Name: Etherscope v1.0
  * Create Date: 23:32:38 02/27/2013
  */
- 
- /* Include Files. */
- 
- /* Standard header files. */
- #include <stdio.h>
- #include <sys/mman.h>
- #include <fcntl.h>
- #include <errno.h>
- #include <unistd.h>
- #include <string.h>
- 
- #include "pru.h"
- 
+
+/* Include Files. */
+
+/* Standard header files. */
+#include <stdio.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "pru.h"
+
 /* Driver header file. */
 #include "prussdrv.h"
 #include <pruss_intc_mapping.h>
